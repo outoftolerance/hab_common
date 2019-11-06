69bb4a0782ea6c7568bfe5cc56a012811ce096f0
James Howard <james@outoftolerance.com>
2019-11-06 08:07:52 -0800

    implement more SMP messages

--- libraries/SimpleMessageProtocol/SMPMessageProtoAck.h
+++ libraries/SimpleMessageProtocol/SMPMessageProtoAck.h
@@ -1,41 +1,23 @@
-#include <SMPCommon.h>
+#define MESSAGE_PROTO_ACK_PAYLOAD_LENGTH 1
+#define MESSAGE_PROTO_ACK_PAYLOAD_BYTE_LENGTH 1
 
-#define MESSAGE_REPORT_ATTITUDE_PAYLOAD_LENGTH 3
-#define MESSAGE_REPORT_ATTITUDE_PAYLOAD_BYTE_LENGTH 12
-
-typedef struct smpMessageReportAttitude
+typedef struct smpMessageProtoAck
 {
-	FloatUnion_t roll;
-	FloatUnion_t pitch;
-	FloatUnion_t heading;
+	uint8_t type;
 };
 
-static inline void smpMessageReportAttitudeEncode(uint8_t node_id, NODE_TYPES node_type, smpMessageReportAttitude& attitude, hdlcMessage& message)
+static inline void smpMessageProtoAckEncode(uint8_t node_id, uint8_t node_type, smpMessageProtoAck& ack, hdlcMessage& message)
 {
 	message.node_id = node_id;
     message.node_type = node_type;
-    message.command = MESSAGE_TYPES::MESSAGE_TYPE_REPORT_ATTITUDE;
-    message.length = MESSAGE_REPORT_ATTITUDE_PAYLOAD_BYTE_LENGTH;
+    message.command = MESSAGE_TYPES::MESSAGE_TYPE_PROTO_ACK;
+    message.length = MESSAGE_PROTO_ACK_PAYLOAD_BYTE_LENGTH;
 
     int data_position = 0;
     int bytes = 0;
 
-    for (bytes = 0; bytes < sizeof(float); bytes++)
+    for (bytes = 0; bytes < sizeof(uint8_t); bytes++)
     {
-        message.payload[data_position + bytes] = attitude.roll.bytes[bytes];
-    }
-
-    data_position += sizeof(float);
-
-    for (bytes = 0; bytes < sizeof(float); bytes++)
-    {
-        message.payload[data_position + bytes] = attitude.pitch.bytes[bytes];
-    }
-
-    data_position += sizeof(float);
-
-    for (bytes = 0; bytes < sizeof(float); bytes++)
-    {
-        message.payload[data_position + bytes] = attitude.heading.bytes[bytes];
+        message.payload[data_position + bytes] = ack.type;
     }
 }
--- libraries/SimpleMessageProtocol/SimpleMessageProtocol.h
+++ libraries/SimpleMessageProtocol/SimpleMessageProtocol.h
@@ -1,5 +1,11 @@
 #include <SimpleHDLC.h>
+
 #include <SMPCommon.h>
+
 #include <SMPMessageHeartbeat.h>
 #include <SMPMessageReportAttitude.h>
 #include <SMPMessageReportPosition.h>
+#include <SMPMessageReportEnvironment.h>
+
+#include <SMPMessageProtoAck.h>
+#include <SMPMessageProtoNack.h>
